251b7b2aaf0203f4594872ae3f716bec585fa927
ze <veepoo_cd@demac-mini.local>
2016-07-21 17:24:54 +0800

    Initial Commit

---
+++ animationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  animationTest
+//
+//  Created by bbigcd on 16/7/21.
+//  Copyright © 2016年 cd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ animationTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  animationTest
+//
+//  Created by bbigcd on 16/7/21.
+//  Copyright © 2016年 cd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
